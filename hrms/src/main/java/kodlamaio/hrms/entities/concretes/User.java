5e242ce5d0082ef0bcf8dcaf96a3bc30d96e612c
Berk Çolak <77544929+berkc@users.noreply.github.com>
2021-05-11 14:32:15 +0300

    Tamamlanancak

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/User.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="email_verified")
+	private boolean emailVerified;
+	
+	
+}
